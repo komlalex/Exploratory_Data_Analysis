fc0e65d13c881f78236045ad0db21318c8f6c255
Alexander Komla <kunalexander360@gmail.com>
2025-04-17 10:26:46 +0000

    Load datasets

--- index.py
+++ index.py
@@ -7,12 +7,33 @@ import pandas as pd
 * survey_results_schema.csv - containing the list of questions (and 
 code for each question)
 * survey_results_public.csv - the full list of responses to the questions
-* so_survey_2020.pdf - the list of questions
 
 Let's load the csv files using the Pandas library. We'll use the name 
 survey_raw_df for the data frame, to indicate that this is unprocessed data which 
 we might clean, filter and modify to prepare a data frame that's ready 
 for data analysis.
 """ 
-survey_raw_df = pd.read_csv("stackoverflow-developer-survey-2020/survey_results_public.csv")
-print(survey_raw_df.shape)
+survey_raw_df = pd.read_csv("stackoverflow-developer-survey-2020/survey_results_public.csv") 
+
+"""The dataset contains over 64,000 responses to 60 questions (although many 
+questions are optional). The responses have been anonymized and there's no 
+personally identifiable information available to us - although each respondent has 
+been assigned a randomized respondent ID. 
+
+Let's view the list of columns"""
+#print(survey_raw_df.columns)
+
+"""It appears that short codes are used as column names. 
+
+We can refer to the schema file to see the full text of each question. The schema file 
+contains only two columns: Column and QurstionText, so we can load it as 
+Pandas Series with Column as the index of the series nad QuestionText as the value. 
+"""
+schema_fname = "stackoverflow-developer-survey-2020/survey_results_schema.csv"
+schema_raw = pd.read_csv(schema_fname, index_col="Column").QuestionText 
+#print(schema_raw) 
+
+"""We can now use schema_raw to retrieve the full question text for any column 
+in surevy_raw_df 
+"""
+print(schema_raw["YearsCodePro"])
